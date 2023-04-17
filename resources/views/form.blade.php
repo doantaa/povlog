bc3610d7e973adb07cc49fe41fb0846566a75c84
doantaa <doanta7aloycius@gmail.com>
2023-04-18 02:27:00 +0700

    finishing form side container

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -2,25 +2,48 @@
 
 @section('container')
     <div class="side-container">
-        <a href="/"><span class="icon material-symbols-outlined">keyboard_backspace</span></a>
-        <h1>FORM PENGAJUAN</h1>
-        <div class="form-section">
-            <div class="mb-3 mt-5">
-                <label for="exampleFormControlInput1" class="form-label">NIM</label>
-                <input class="form-control" id="exampleFormControlInput1" placeholder="Masukkan NIM Anda">
+        <a href="/"><span class="icon material-symbols-rounded">keyboard_backspace</span></a>
+        <h1 class="form-title" style="font-family: 'Poppins">FORM PENGAJUAN</h1>
+
+        <form action="">
+            <div class="form-nim my-5">
+                <label for="">NIM</label><br>
+                <input type="text" class="form-control" placeholder="Masukkan NIM">
             </div>
 
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">NIM</label>
-                <input class="form-control" id="exampleFormControlInput1" placeholder="Masukkan NIM Anda">
+            <div class="form-name my-5 pt-5">
+                <label for="">Nama Lengkap</label>
+                <input type="text" class="form-control" placeholder="Masukkan Nama Lengkap">
             </div>
-        </div>
+
+            <div class="form-keperluan my-5 pt-5">
+                <label for="">Keperluan</label>
+                <input type="text" class="form-control" placeholder="contoh: Sosialisasi HMIF" > </textarea>
+            </div>
+
+            <div class="form-tanggal my-5 pt-5">
+                <label class="mb-3">Tanggal Peminjaman</label>
+                <div class="input-group date" id="tanggal-pinjam">
+                    <input type="text" class="form-control">
+                    <span class="input-group-addon material-symbols-rounded" style="transform: scale(2.5); background:none; border: 0">
+                        date_range
+                    </span>
+                </div>
+            </div>
+
+            <div class="form-tanggal my-5 pt-5">
+                <label class="mb-3">Tanggal Pengembalian</label>
+                <div class="input-group date" id="tanggal-kembali">
+                    <input type="text" class="form-control">
+                    <span class="input-group-addon material-symbols-rounded" style="transform: scale(2.5); background:none; border: 0">
+                        date_range
+                    </span>
+                </div>
+            </div>
+        </form>        
     </div>
 
-
-
-
-    
 @endsection
+
 
 
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,21 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title }} </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,600,0,0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css"> 
   </head>
+
   <body>
-
-
     @yield('container')
     @yield('script')
-    
-
-
+  
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/js/bootstrap-datetimepicker.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/js/bootstrap-datetimepicker.min.js"></script>
+
+    {{-- DATE TIME PICKER SCRIPT --}}
+    <script type="text/javascript">
+      $(function() {
+         $('#tanggal-pinjam').datetimepicker();
+         $('#tanggal-kembali').datetimepicker();
+  
+      });
+  </script> 
   </body>
   
 </html>
