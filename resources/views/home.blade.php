70f3e75ec010c2c624ed08595ddbedc4cbe55a22
doantaa <doanta7aloycius@gmail.com>
2023-05-01 22:24:40 +0700

    create first block home layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,44 @@
 @extends('layouts/main')
-@include('partials/navbar')
-@section('container')
-    <h1>Home</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body class="body">
+    
+    {{-- @include('partials/navbar') --}}
 
-@endsection
+    <header class="container">
+        <a href="/"><img src="img/logo.png" class="image-logo" alt="poving-logistic"></a>
+        
+        <p class="info-text"><img src="img/circle.png" width="12px" alt=""> DATANGLAH SAAT JAM KERJA (08:00 - 16.00)</p>
+    </header>
+
+    <div class="main-layout container">
+        <div class="left-layout">
+            <div class="main-title">
+                Pinjam kebutuhan acaramu dengan mudah lewat Poving Logistic
+            </div> 
+
+            <div class="main-desc">
+                Platform peminjaman sarana dan prasarana yang mudah digunakan. Tidak perlu buat akun. Eksklusif untuk sivitas akademika IT Telkom Purwokerto.
+            </div>
+
+            <div class="btn btn-block btn-danger">
+                Ajukan Peminjaman
+            </div>
+        </div>
+
+        <div class="right-layout">
+            <img src="img/main-img.png" class="main-image" alt="image">
+
+        </div>
+
+    </div>
+
+</body>
+</html>
